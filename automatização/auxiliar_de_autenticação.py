0d6b6b5f9dd54cb25272b6e5c93c55279ada6024
Christopher Gabriel Marques <christophermarques75@gmail.com>
2026-08-26 20:21:31 -0300

    add automatizacao_salafuturo

--- automatização/auxiliar_de_autenticação.py
+++ automatização/auxiliar_de_autenticação.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 import keyboard
 import sys
+from webbrowser import open
 
 def automatizar_001():
     if keyboard.is_pressed("j"):
@@ -24,3 +25,13 @@ def automatizar_002():
     time.sleep(1)
     pyautogui.press("enter")
 
+def automatizar_o_salafuturo():
+    open("https://saladofuturo.educacao.sp.gov.br/login-alunos")
+    keyboard.wait("esc")
+    pyautogui.press("tab", presses=19)
+    pyautogui.write("seu ra")
+    pyautogui.press("tab")
+    pyautogui.write("seu digito")
+    pyautogui.press("tab", presses=2)
+    pyautogui.write("s")
+    pyautogui.press("enter")
