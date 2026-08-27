d75ab579580b81685e12a01307258d04248e25a8
Christopher Gabriel Marques <christophermarques75@gmail.com>
2026-08-27 15:56:39 -0300

    utilizando corretamente funções

--- codigos-de-python/codigos de aprendizado/aprendendo_função.py
+++ codigos-de-python/codigos de aprendizado/aprendendo_função.py
@@ -1,31 +1,39 @@
 def calcular_imposto(salario):
     salario = float(salario)
     if salario <= 1518:
-        impostado =  salario * 0.075
-        valor_descontato = salario + impostado 
-        print(f"seu salario de {salario} virou {valor_descontato:.2f} se não tivesse o desconto de 7,5%")
+        imposto_atual = 0.075
+        desconto =  salario * imposto_atual
+        valor_descontato = salario + desconto 
+        imposto_atual = imposto_atual * 100
 
     elif salario > 1518 and salario < 2793.88:
-        impostado =  salario * 0.09
-        valor_descontato = salario + impostado 
-        print(f"seu salario de {salario} virou {valor_descontato:.2f} se não tivesse o desconto de 9%")
+        imposto_atual = 0.09
+        desconto =  salario * imposto_atual
+        valor_descontato = salario + desconto 
+        imposto_atual = imposto_atual * 100
 
     elif salario > 2793.89 and salario < 4190.83:
-        impostado =  salario * 0.12
-        valor_descontato = salario + impostado 
-        print(f"seu salario de {salario} virou {valor_descontato:.2f} se não tivesse o desconto de 12%")
+        imposto_atual = 0.12
+        desconto =  salario * imposto_atual
+        valor_descontato = salario + desconto 
+        imposto_atual = imposto_atual * 100
     
     elif salario > 4190.84 and salario < 8157.41:
-        impostado =  salario * 0.14
-        valor_descontato = salario + impostado 
-        print(f"seu salario de {salario} virou {valor_descontato:.2f} se não tivesse o desconto de 13%")
+        imposto_atual = 0.14
+        desconto =  salario * 0.14
+        valor_descontato = salario + desconto 
+        imposto_atual = imposto_atual * 100
     
-    return valor_descontato
+    return valor_descontato, imposto_atual
+
 user = input("qual o valor do seu salario: ")
 
 try:
     salario_num = float(user.replace(",", "."))
-    calcular_imposto(user)
+    desconto = calcular_imposto(user)[0]
+    imposto = calcular_imposto(user)[1]
+    print(f"seu salario seria {desconto} sem o imposto de {imposto:.1f}% do governo")
+
 
 except ValueError:
     print("Somente valores numéricos, bro.")
