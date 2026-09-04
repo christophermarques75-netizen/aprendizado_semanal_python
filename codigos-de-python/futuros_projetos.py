7cf2e1c931461f1e4751fc17ada0e6caf28f245d
Christopher Gabriel Marques <christophermarques75@gmail.com>
2026-09-04 17:03:53 -0300

    atualização_02

--- codigos-de-python/futuros_projetos.py
+++ codigos-de-python/futuros_projetos.py
@@ -1,13 +1,13 @@
 user = int(input("valor inicial do investimento:  "))
-investimento_mensal = int(input("valor investimento mensalmente: "))
+valor_investimento_mensal = int(input("valor investimento mensalmente: "))
 imposto_memsal = 0.019
 tempo = int(input("valor em meses: "))
 
 for tempo in range(1, tempo + 1):
-    user += investimento_mensal
+    user += valor_investimento_mensal
     user = user * (1 + imposto_memsal)
 
-diferença = user - investimento_mensal
+diferença = user - valor_investimento_mensal
 
 print(f'{diferença:.2f}')
 print(f"{user:.2f}")
