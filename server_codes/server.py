c32e16e7117e879cced6026fafd8a5c40dfff068
Christopher Gabriel Marques <christophermarques75@gmail.com>
2026-09-10 21:04:27 -0300

    docs: add flask and javascript

--- server_codes/server.py
+++ server_codes/server.py
@@ -1,12 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-@app.route('/receber-dados', methods=['POST'])
-def receber_dados():
-    dados = request.get_json()
-    print("Dados recebidos:", dados)
-    return jsonify({"status": "sucesso", "mensagem": "Dados salvos!"})
+@app.route('/', methods=['GET', 'POST'])
+
+def jogo():
+
+    resultado = ""
+    
+    if request.method == "POST":
+
+        jogador = request.form.get('jogada').upper()
+        computador = random.choice(['PEDRA', 'PAPEL', 'TESOURA'])
+
+        if jogador == computador:
+            resultado  = f"empate, ambos escolheram {jogador}."
+        elif (jogador == "PEDRA" and computador == "TESOURA") or (jogador == "PAPEL" and computador == "PEDRA") or (jogador == "TESOURA" and computador == "PAPEL"):
+            resultado = f"voce ganhou pq computador escolheu {computador}"
+        else:
+            resultado = f"voce perdeu pq computador escolheu {computador}"
+            
+    return render_template("interface.html", resultado=resultado)
 
 if __name__ == '__main__':
     app.run(port=5000)
